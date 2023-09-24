0f755ac28883c425e3793cc96029b329a32831bd
Merlono <merlonotk@gmail.com>
2023-09-24 19:53:23 -0400

    Changed File names.

---
+++ src/main/java/net/merlono/merlonositems/item/ModCreativeModTabs.java
@@ -0,0 +1,34 @@
+package net.merlono.merlonositems.item;
+
+import net.merlono.merlonositems.MerlonosItems;
+import net.merlono.merlonositems.block.ModBlocks;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModCreativeModTabs {
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MerlonosItems.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> MERLONOS_WEAPONS = CREATIVE_MODE_TABS.register("merlonos_items",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(Moditems.STAILIUM.get()))
+                    .title(Component.translatable("creativetab.merlonos_items"))
+                    .displayItems((p_270258_, p_259752_) -> {
+                        p_259752_.accept(Moditems.STAILIUM.get());
+                        p_259752_.accept(Moditems.RAW_STAILIUM.get());
+                        p_259752_.accept(Moditems.HILT.get());
+                        p_259752_.accept(ModBlocks.STAILIUM_BLOCK.get());
+
+
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+
+}
---
+++ src/main/java/net/merlono/merlonositems/item/Moditems.java
@@ -0,0 +1,25 @@
+package net.merlono.merlonositems.item;
+
+import net.merlono.merlonositems.MerlonosItems;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class Moditems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MerlonosItems.MOD_ID);
+
+    public static final RegistryObject<Item> STAILIUM = ITEMS.register( "stailium",
+            () ->  new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_STAILIUM = ITEMS.register( "raw_stailium",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> HILT = ITEMS.register( "hilt",
+            () ->  new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
